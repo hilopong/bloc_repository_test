6ab5f324c096b5ab4646eab74a7cb28e0a518bc0
김윤성 <hilopong@hotmail.com>
2022-05-27 16:54:37 +0900

    futerbuilder complete

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -9,10 +8,10 @@ import '../widgets/product_card.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     Future<List<Product>> getprdlist(int catecode) async {
+
       var result = await http.get(Uri.parse(
           'https://webapi.superdesk.co.kr/AdminProduct/GetPrdList/?companyidx=' +
               catecode.toString()));
@@ -21,9 +20,12 @@ class HomePage extends StatelessWidget {
       List<Product> retdata = [];
 
       for (int i = 0; i < result2.length; i++) {
-        Product prdval = Product(I_idx: result2[i]['I_idx'],
+        Product prdval = Product(
+            I_idx: result2[i]['I_idx'],
             NVC_prdName: result2[i]['NVC_prdName'],
-            NVC_ImgName: "https://webapi.superdesk.co.kr/Content/Prd/PrdOrigin/"+result2[i]['NVC_ImgName'],
+            NVC_ImgName:
+                "https://webapi.superdesk.co.kr/Content/Prd/PrdOrigin/" +
+                    result2[i]['NVC_ImgName'],
             I_discountPrice: result2[i]['I_discountPrice']);
         retdata.add(prdval);
       }
@@ -33,39 +35,43 @@ class HomePage extends StatelessWidget {
 
     return FutureBuilder(
       future: getprdlist(36),
-      builder: (context,AsyncSnapshot<List<Product>> snapshot){
-        if(snapshot.hasData){
+      builder: (context, AsyncSnapshot<List<Product>> snapshot) {
+        if (snapshot.hasData) {
           return Scaffold(
             body: SafeArea(
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 50),
-                child:
-                SizedBox(
+                child: SizedBox(
                     height: 165,
                     child: ListView.builder(
                         scrollDirection: Axis.horizontal,
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 10.0),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 10.0),
                         shrinkWrap: true,
                         itemCount: snapshot.data!.length,
-                        itemBuilder: (context,index){
+                        itemBuilder: (context, index) {
                           return Padding(
                             padding: const EdgeInsets.only(right: 5.0),
                             child: ProductCard(product: snapshot.data![index]),
                           );
-                        })
-                ),
+                        })),
                 //ProductCard(product: Product.products[0])
               ),
             ),
           );
-        }else{
-          return Center(child: CircularProgressIndicator());
+        } else {
+          return Scaffold(
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50),
+                child: Center(child: CircularProgressIndicator()),
+              ),
+            ),
+          );
+          // return Center(child: CircularProgressIndicator());
         }
       },
     );
-
-
-
 
     // return Scaffold(
     //   body: GridView.builder(
@@ -81,7 +87,5 @@ class HomePage extends StatelessWidget {
     //       }
     //   ),
     // );
-
-
   }
 }
